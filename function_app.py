b79a211c287f88e56de21ccbffa27f339f24c1a4
CarlosMg <carlos_mgar@hotmail.com>
2024-09-29 12:36:59 -0600

    se agrega ws data

--- function_app.py
+++ function_app.py
@@ -8,21 +8,16 @@ def send_whatsapp_message(phone_number, message):
     import time
     import os
 
-    user_data_dir = "whatsapp_data"
-    if not os.path.exists(user_data_dir):
-        os.makedirs(user_data_dir)
-        logging.info(f"Directorio '{user_data_dir}' creado.")
-
     try:
         with sync_playwright() as p:
             try:
                 
-                browser = p.chromium.launch_persistent_context(user_data_dir=user_data_dir, headless=False)
+                browser = p.chromium.launch_persistent_context(user_data_dir='ws_data', headless=False)
             except Exception as e:
                 logging.error(f"Error launching browser: {e}")
                 logging.info("Attempting to install Playwright browsers...")
                 
-                browser = p.chromium.launch_persistent_context(user_data_dir=user_data_dir, headless=False)
+                browser = p.chromium.launch_persistent_context(user_data_dir='ws_data', headless=False)
 
             page = browser.new_page()
             whatsapp_url = f"https://web.whatsapp.com/send?phone={phone_number}&text={message}"
